2f18e01936524a53150a3f039c92209f1083af04
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-27 14:54:47 +0200

    feat(): add action to todo button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,11 +3,15 @@
  * @returns Todo JSX element
  */
 function Todo(props) {
+    function deleteHandler() {
+        alert('deleted')
+    }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Click me</button>
+        <button className="btn" onClick={deleteHandler}>Click me</button>
       </div>
     </div>
   );
