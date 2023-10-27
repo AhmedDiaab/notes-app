1b42192252f8cc0b099855cda807781dabce2910
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-27 14:38:41 +0200

    feat(): pass dynamic content to todo withh types

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,11 @@
-function Todo() {
+/**
+ * @param {import("../types/todo.props").TodoProps} props properties passed to component
+ * @returns Todo JSX element
+ */
+function Todo(props) {
   return (
     <div className="card">
-      <h2>Title</h2>
+      <h2>{props.text}</h2>
       <div className="actions">
         <button className="btn">Click me</button>
       </div>
---
+++ src/types/todo.props.d.ts
@@ -0,0 +1,3 @@
+export type TodoProps = {
+    text: string;
+}
