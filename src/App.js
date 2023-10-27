fa1a919a145d445a49b2e8eecfe047f208783669
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-27 15:11:27 +0200

    feat(): create modal and backdrop components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 // React elements must start with capital letter
 // this is how react deferentiates between native html elements 
@@ -9,6 +11,8 @@ function App() {
       <Todo text="Learn React"/> 
       <Todo text="Learn Next"/> 
       <Todo text="Create Application"/> 
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Modal() {
+  return <div className="modal">
+    <p>Are you sure?</p>
+    <button className="btn">Cancel</button>
+    <button className="btn brn-alt">Confirm</button>
+  </div>;
+};
+
+export default Modal;
