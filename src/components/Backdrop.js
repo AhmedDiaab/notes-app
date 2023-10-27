4b9672dca448ddc499febaed938ed2f0c0dda595
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-27 15:49:57 +0200

    feat(): pass backdrop props and create prop types

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Backdrop() {
-  return <div className="backdrop" />;
+/**
+ *
+ * @param {import("../types/backdrop.props").BackdropProps} props
+ */
+function Backdrop(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+
+  return <div className="backdrop" onClick={cancelHandler} />;
 }
 
 export default Backdrop;
