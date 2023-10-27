6f12d27025d749f8270b3d1cac6db44188caa4db
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2023-10-27 14:26:17 +0200

    feat(): create first elementt and its styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-
+import Todo from "./components/Todo";
+// React elements must start with capital letter
+// this is how react deferentiates between native html elements 
+// and custom elements
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo /> 
+    </div>
   );
 }
 
